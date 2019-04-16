b65d6b3ac4c5791a27bb7b9b312428c62f682919
Edwar Jhoel <49489730+DOTA2normalin@users.noreply.github.com>
2019-04-16 10:59:31 -0500

    Add files via upload

---
+++ ass4.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sum(const int x, const int y, const int z)
+{
+	return x+y+z;
+}
+
+int sum(const int a,const int b,const int c,const int d)
+{
+	return a+b+c+d;
+}
+
+int main()
+{
+	cout<<sum(2,3,4);
+	cout<<sum(1,2,3,4);
+	return 0;
+}
